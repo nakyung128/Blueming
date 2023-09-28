d16b5c6fdda2be591cdad7fa8e1958d49c2e3951
nakyung128 <lnk12888@naver.com>
2023-09-29 01:58:24 +0900

    🚑 TabBar Font 버그 수정

--- Blueming/Sources/TabBarViewController.swift
+++ Blueming/Sources/TabBarViewController.swift
@@ -12,7 +12,33 @@ class TabBarViewController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        if #available(iOS 13.0, *) {
+            let appearance = UITabBarAppearance()
+            
+            // 선택되지 않은 상태의 폰트 및 자간 설정
+            let normalFont = UIFont(name: "Pretendard-Medium", size: 12)
+            let normalSpacing: CGFloat = -0.6
+            
+            let normalAttributes: [NSAttributedString.Key: Any] = [
+                .font: normalFont as Any,
+                .kern: normalSpacing
+            ]
+            
+            appearance.stackedLayoutAppearance.normal.titleTextAttributes = normalAttributes
+            
+            // 선택된 상태의 폰트 및 자간 설정
+            let selectedFont = UIFont(name: "Pretendard-Medium", size: 12)
+            let selectedSpacing: CGFloat = -0.6
+            
+            let selectedAttributes: [NSAttributedString.Key: Any] = [
+                .font: selectedFont as Any,
+                .kern: selectedSpacing
+            ]
+            
+            appearance.stackedLayoutAppearance.selected.titleTextAttributes = selectedAttributes
+            
+            self.tabBar.standardAppearance = appearance
+        }
     }
 
 }
