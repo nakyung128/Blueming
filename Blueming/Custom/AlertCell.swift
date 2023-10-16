b065ce8a02f3e0d18fae2858c5f9bf336771d850
nakyung128 <lnk12888@naver.com>
2023-10-16 21:39:48 +0900

    ✨ 알림창 tableView 구현

---
+++ Blueming/Custom/AlertCell.swift
@@ -0,0 +1,23 @@
+//
+//  AlertCell.swift
+//  Blueming
+//
+//  Created by 이나경 on 10/16/23.
+//
+
+import UIKit
+
+class AlertCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ Blueming/Model/Alert.swift
@@ -0,0 +1,8 @@
+//
+//  Alert.swift
+//  Blueming
+//
+//  Created by 이나경 on 10/16/23.
+//
+
+import Foundation
