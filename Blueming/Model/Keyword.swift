8f1e64634b8864fd541947fe20ae080b2f688dea
nakyung128 <lnk12888@naver.com>
2023-11-01 03:11:38 +0900

    ✨ 키워드 이미지 변경

--- Blueming/Model/Keyword.swift
+++ Blueming/Model/Keyword.swift
@@ -29,38 +29,38 @@ extension Keyword {
     ]
     
     static var emotion = [
-        Keyword(img: "emotion_example", keyword: "감정기복"),
-        Keyword(img: "emotion_example", keyword: "두려움"),
-        Keyword(img: "emotion_example", keyword: "무기력"),
-        Keyword(img: "emotion_example", keyword: "불안/초조"),
-        Keyword(img: "emotion_example", keyword: "스트레스"),
-        Keyword(img: "emotion_example", keyword: "슬픔"),
-        Keyword(img: "emotion_example", keyword: "죄책감"),
-        Keyword(img: "emotion_example", keyword: "예민/짜증"),
-        Keyword(img: "emotion_example", keyword: "우울"),
-        Keyword(img: "emotion_example", keyword: "절망감"),
-        Keyword(img: "emotion_example", keyword: "혼란"),
-        Keyword(img: "emotion_example", keyword: "분노")
+        Keyword(img: "감정기복", keyword: "감정기복"),
+        Keyword(img: "두려움", keyword: "두려움"),
+        Keyword(img: "무기력", keyword: "무기력"),
+        Keyword(img: "불안초조", keyword: "불안/초조"),
+        Keyword(img: "스트레스", keyword: "스트레스"),
+        Keyword(img: "슬픔", keyword: "슬픔"),
+        Keyword(img: "죄책감", keyword: "죄책감"),
+        Keyword(img: "예민짜증", keyword: "예민/짜증"),
+        Keyword(img: "우울", keyword: "우울"),
+        Keyword(img: "절망감", keyword: "절망감"),
+        Keyword(img: "혼란", keyword: "혼란"),
+        Keyword(img: "분노", keyword: "분노")
     ]
     
     static var health = [
-        Keyword(img: "emotion_example", keyword: "건망증"),
-        Keyword(img: "emotion_example", keyword: "두통/어지럼증"),
-        Keyword(img: "emotion_example", keyword: "위장장애"),
-        Keyword(img: "emotion_example", keyword: "메스꺼움"),
-        Keyword(img: "emotion_example", keyword: "관절통"),
-        Keyword(img: "emotion_example", keyword: "피로"),
-        Keyword(img: "emotion_example", keyword: "부종"),
-        Keyword(img: "emotion_example", keyword: "배변 장애"),
-        Keyword(img: "emotion_example", keyword: "수면 장애"),
-        Keyword(img: "emotion_example", keyword: "식욕 변화"),
-        Keyword(img: "emotion_example", keyword: "외상"),
-        Keyword(img: "emotion_example", keyword: "요통"),
-        Keyword(img: "emotion_example", keyword: "젖몸살"),
-        Keyword(img: "emotion_example", keyword: "질분비물"),
-        Keyword(img: "emotion_example", keyword: "탈모"),
-        Keyword(img: "emotion_example", keyword: "복통"),
-        Keyword(img: "emotion_example", keyword: "피부질환"),
-        Keyword(img: "emotion_example", keyword: "훗배앓이")
+        Keyword(img: "건망증", keyword: "건망증"),
+        Keyword(img: "두통", keyword: "두통/어지럼증"),
+        Keyword(img: "위장장애", keyword: "위장장애"),
+        Keyword(img: "메스꺼움", keyword: "메스꺼움"),
+        Keyword(img: "관절통", keyword: "관절통"),
+        Keyword(img: "피로", keyword: "피로"),
+        Keyword(img: "부종", keyword: "부종"),
+        Keyword(img: "배변장애", keyword: "배변 장애"),
+        Keyword(img: "수면장애", keyword: "수면 장애"),
+        Keyword(img: "식욕변화", keyword: "식욕 변화"),
+        Keyword(img: "외상", keyword: "외상"),
+        Keyword(img: "요통", keyword: "요통"),
+        Keyword(img: "젖몸살", keyword: "젖몸살"),
+        Keyword(img: "질분비물", keyword: "질분비물"),
+        Keyword(img: "탈모", keyword: "탈모"),
+        Keyword(img: "복통", keyword: "복통"),
+        Keyword(img: "피부질환", keyword: "피부질환"),
+        Keyword(img: "훗배앓이", keyword: "훗배앓이")
     ]
 }
